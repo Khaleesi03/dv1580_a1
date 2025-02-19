1fc9730b6103b4c6a81003a3119dc771c5df0b17
Khaleesi03 <fmozse@gmail.com>
2025-02-19 13:56:07 +0100

    move if statement

--- memory_manager.c
+++ memory_manager.c
@@ -30,14 +30,14 @@ void mem_init(size_t size) {
     free_list->size = size - BLOCK_HEADER_SIZE;
     free_list->free = 1;
     free_list->next = NULL;
+    if (size > memory_pool_size * 0.2) {
+        fprintf(stderr, "Requested memory size exceeds 20%% of the memory pool size\n");
+        exit(EXIT_FAILURE);
+    }
 }
-if (size > memory_pool_size * 0.2) {
-    fprintf(stderr, "Requested memory size exceeds 20%% of the memory pool size\n");
-    exit(EXIT_FAILURE);
-}
+
 void* mem_alloc(size_t size) {
     BlockHeader* current = free_list;
-    BlockHeader* previous = NULL;
 
     while (current != NULL) {
         if (current->free && current->size >= size) {
