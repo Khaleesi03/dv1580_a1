ba17142ded2f28fb55656f24d5c0234eed1ad83d
Khaleesi03 <fmozse@gmail.com>
2025-02-26 13:19:03 +0100

    mm

--- memory_manager.c
+++ memory_manager.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <sys/mman.h> // For mmap and munmap
 
 typedef struct BlockHeader {
+    size_t offest;              //offset from beginning of memory pool
     size_t size;               // Size of the block (excluding header)
     int free;                 // 1 if the block is free, 0 if allocated
     struct BlockHeader* next;  // Pointer to the next block in the free list
 } BlockHeader;
-
-#define PAGE_SIZE 5096
 
 static void* memory_pool = NULL;  // Pointer to the memory pool
 static size_t memory_pool_size = 0; // Total size of the memory pool
@@ -18,28 +16,20 @@ static BlockHeader* free_list = NULL; // Head of the free list
 
 #define BLOCK_HEADER_SIZE sizeof(BlockHeader)
 
-// Initialize the memory manager with a given pool size
 void mem_init(size_t size) {
-    if(size >= PAGE_SIZE){
-        memory_pool = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        if (memory_pool == MAP_FAILED) {
-            fprintf(stderr, "Failed to initialize memory pool\n");
-            exit(EXIT_FAILURE);
-        } 
-    } else {
-        memory_pool = malloc(size);
-        if (!memory_pool){
-            perror("malloc failed");
-            exit(1);
-        }
-        
+    memory_pool = malloc(size);
+    if (!memory_pool) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
     }
     memory_pool_size = size;
+
     free_list = (BlockHeader*)memory_pool;
     free_list->size = memory_pool_size - sizeof(BlockHeader);
     free_list->free = 1;
     free_list->next = NULL;
 }
+
 
 void *mem_alloc(size_t size) {
     BlockHeader *current = free_list;
@@ -119,15 +109,17 @@ void* mem_resize(void* block, size_t size) {
 }
 
 void mem_deinit() {
-    if (free_list != NULL) {
-        fprintf(stderr, "Memory leak detected: free list is not empty.\n");
+    if (memory_pool){
+        free(memory_pool);
+        memory_pool = NULL;
+        memory_pool_size = 0;
     }
+
     BlockHeader* current = free_list;
-    while (current) {
-        printf("Free list contains block at %p, size: %zu\n", current, current->size);
-        current = current->next;
+    while (current){
+        BlockHeader* next = current -> next;
+        free(current);
+        current = next;
     }
-    munmap(memory_pool, memory_pool_size);
-    memory_pool = NULL;
-    memory_pool_size = 0;
+    free_list = NULL;
 }
